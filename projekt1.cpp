21ad5763e63c097028948932c39661c399b2399f
korczyk <kamil.orczyk@op.pl>
2024-03-10 13:49:33 +0100

    added factorial usage for user

--- projekt1.cpp
+++ projekt1.cpp
@@ -25,8 +25,22 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Silnia" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+	switch(wyjscie) {
+		case 0:
+			break;
+		case 1:
+			int a;
+			cout << "Podaj liczbe naturalna: ";
+			cin >> a;
+			silnia(a);	
+			break;
+		default:
+			cout << "Zly wybor" << endl;
+			break;
+	}
     } while(wyjscie != 0);
     return 0;
 }
